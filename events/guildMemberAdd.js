ad9275583ce99614c96235c3e1bd19f7b2d82360
Akimoto Ryou <akimoto.ryou@protonmail.com>
2019-02-04 06:27:50 +0700

    make it better

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,3 @@
 module.exports = (client, member) => {
-  member.send('Welcome to VictoriousReturn server! Please follow the rules at #rules channel to make a nice environment for all of us :)')
+  member.send('Welcome to VictoriousReturn server!\n Please follow the rules at #rules channel to make a nice environment for all of us :)\n\nRegards, \n\nDiscord Admin')
 }
