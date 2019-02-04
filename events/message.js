735a55b366d415b8d91c4e8bd31272c9d1be15bb
Akimoto Ryou <akimoto.ryou@protonmail.com>
2019-02-04 08:12:50 +0700

    make it better

--- events/message.js
+++ events/message.js
@@ -1,11 +1,14 @@
 const {prefix} = require('../config.json')
 const kick = require('../commands/kick')
 const ban = require('../commands/ban')
+const info = require('../commands/info')
 
 module.exports = (client, message) => {
   if(message.content.startsWith(`${prefix}kick`)){
     return kick(message)
   }else if(message.content.startsWith(`${prefix}ban`)){
     return ban(message)
+  }else if(message.content(`${prefix}info`)){
+    return info(message)
   }
 }
