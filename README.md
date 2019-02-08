06b8ff106550418dc7484d5378c3af508dfe703f
Akimoto Ryou <akimoto.ryou@protonmail.com>
2019-02-08 10:01:46 +0700

    .gitignore is now working

--- README.md
+++
@@ -1,6 +0,0 @@
-# ViRe-Bot
-Prefix : v[space] or @mention[space]
-
-Command list : help, info, kick, ban, prune
-
-Note : To add commands, add .js file at commands folder no need to change other files.
--- commands/info.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name : 'info',
-  execute(message){
-    return message.channel.send('VictoriousReturn Clan\n\nSocial Media :\n- Guilded : http://guilded.gg/ViRe\n- Discord : http://discord.gg/meu46Vt\n- Facebook : http://www.facebook.com/VictoriousReturn/?ref=br_rs\n- Youtube : http://m.youtube.com/channel/UCz6h1Xcj3zZ2Oq8sSoirppQ')
-  },
-}
